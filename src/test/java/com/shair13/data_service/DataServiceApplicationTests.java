cabc3156b8b8989d1db4e027f130712a33837227
Szer13 <shad350@gmail.com>
2024-08-10 10:29:22 +0200

    first commit

---
+++ src/test/java/com/shair13/data_service/DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shair13.data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
