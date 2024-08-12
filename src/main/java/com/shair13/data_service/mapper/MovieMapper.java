1c93807b5b04dcd93c9c59bb1eca0a53b62e9912
Szer13 <shad350@gmail.com>
2024-08-12 17:24:40 +0200

    added mapstruct unit test

--- src/main/java/com/shair13/data_service/mapper/MovieMapper.java
+++ src/main/java/com/shair13/data_service/mapper/MovieMapper.java
@@ -4,10 +4,13 @@ import com.shair13.data_service.dto.PagedMovie;
 import com.shair13.data_service.entity.Movie;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
+
+    MovieMapper INSTANCE = Mappers.getMapper( MovieMapper.class );
 
     @Mapping(source = "content", target = "movies")
     @Mapping(source = "number", target = "pageNumber")
--- src/test/java/com/shair13/data_service/mapper/MovieMapperTest.java
+++ src/test/java/com/shair13/data_service/mapper/MovieMapperTest.java
@@ -1,11 +1,34 @@
 package com.shair13.data_service.mapper;
 
+import com.shair13.data_service.dto.PagedMovie;
+import com.shair13.data_service.entity.Movie;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieMapperTest {
 
     @Test
     void pageToPagedMovie() {
+        // given
+        int pageNumber = 0;
+        int size = 10;
+        Pageable pageable = PageRequest.of(pageNumber, size, Sort.by("id"));
+
+        Movie movie = new Movie(1L, "title", "director", "description", 5.0);
+        List<Movie> movies = List.of(movie);
+
+        Page<Movie> moviePage = new PageImpl<>(movies, pageable, 2);
+
+        // when
+        PagedMovie pagedMovie = MovieMapper.INSTANCE.pageToPagedMovie(moviePage);
+
+        // then
+        assertEquals(movies.size(), pagedMovie.getMovies().size());
+        assertEquals(pageNumber, pagedMovie.getPageNumber());
+        assertEquals(size, pagedMovie.getPageSize());
     }
 }
