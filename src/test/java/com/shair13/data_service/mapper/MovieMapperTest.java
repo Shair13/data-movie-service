33b31e110ac73c0c428f59aa07dd23abfecd549b
Szer13 <shad350@gmail.com>
2024-08-12 17:23:49 +0200

    added mapstruct unit test

---
+++ src/test/java/com/shair13/data_service/mapper/MovieMapperTest.java
@@ -0,0 +1,11 @@
+package com.shair13.data_service.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MovieMapperTest {
+
+    @Test
+    void pageToPagedMovie() {
+    }
+}
